ae8b278830f6111352a8db86bc1ae82dfb1c051a
ThisIsSandeep <thesandybansal@gmail.com>
2018-09-21 12:03:54 +0530

    Middleware function :stuck_out_tongue_closed_eyes: :stuck_out_tongue_closed_eyes:

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+// Middleware function
+app.use(logger);
 
 courses = [
   { id: 1, name: 'Node js' },
